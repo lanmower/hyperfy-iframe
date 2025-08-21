5a22af9900f4f61c10b3b4aeb9bd21f08b324815
Ash <hello@ashconnell.com>
2025-08-21 21:54:24 +1000

    fix: prim memory leak + another big performance boost

--- src/core/nodes/Prim.js
+++ src/core/nodes/Prim.js
@@ -12,7 +12,7 @@ const defaults = {
   size: null,
   color: '#ffffff',
   emissive: null,
-  emissiveIntensity: 1,
+  emissiveIntensity: 0,
   metalness: 0.2,
   roughness: 0.8,
   opacity: 1,
@@ -115,7 +115,7 @@ const materialCache = new Map()
 // Create material with specific properties
 const getMaterial = props => {
   // Create a cache key from material properties
-  const cacheKey = `${props.emissive}_${props.emissiveIntensity}_${props.metalness}_${props.roughness}_${props.opacity}_${props.texture}_${props.doubleside}`
+  const cacheKey = `${props.metalness}_${props.roughness}_${props.opacity}_${props.texture}_${props.doubleside}`
 
   // Check cache first
   if (materialCache.has(cacheKey)) {
@@ -124,8 +124,8 @@ const getMaterial = props => {
 
   const material = new THREE.MeshStandardMaterial({
     color: 0xffffff,
-    emissive: props.emissive,
-    emissiveIntensity: props.emissiveIntensity,
+    emissive: 0x000000,
+    emissiveIntensity: 0,
     metalness: props.metalness,
     roughness: props.roughness,
     opacity: props.opacity,
@@ -143,6 +143,13 @@ const getMaterial = props => {
   materialCache.set(cacheKey, material)
 
   return material
+}
+
+if (typeof window !== 'undefined') {
+  window.primCache = {
+    material: materialCache,
+    geometry: geometryCache,
+  }
 }
 
 const applyTexture = (material, textureUrl, loader) => {
@@ -224,11 +231,11 @@ export class Prim extends Node {
       // Create material with current properties
       const material = getMaterial({
         // color: this._color,
-        emissive: this._emissive,
-        emissiveIntensity: this._emissiveIntensity,
+        // emissive: this._emissive,
+        // emissiveIntensity: this._emissiveIntensity,
         metalness: this._metalness,
         roughness: this._roughness,
-        opacity: this._opacity,
+        opacity: quantizeOpacity(this._opacity), // reduce material variations
         texture: this._texture,
         doubleside: this._doubleside,
       })
@@ -243,6 +250,7 @@ export class Prim extends Node {
       this.handle = this.ctx.world.stage.insertLinked({
         geometry,
         material,
+        uberShader: true,
         castShadow: this._castShadow,
         receiveShadow: this._receiveShadow,
         matrix: this.matrixWorldOffset,
@@ -251,6 +259,8 @@ export class Prim extends Node {
       })
       // console.log('FOO', this._color)
       this.handle.setColor(this._color)
+      this.handle.setEmissive(this._emissive)
+      this.handle.setEmissiveIntensity(this._emissiveIntensity)
     }
 
     // Create physics if enabled
@@ -500,18 +510,18 @@ export class Prim extends Node {
     this.matrixWorldOffset.compose(_v1, _q1, _v2)
   }
 
-  applyStats(stats) {
-    const geometry = getGeometry(this._type)
-    if (geometry && !stats.geometries.has(geometry.uuid)) {
-      stats.geometries.add(geometry.uuid)
-      stats.triangles += getTrianglesFromGeometry(geometry)
-    }
-    const material = getMaterial()
-    if (material && !stats.materials.has(material.uuid)) {
-      stats.materials.add(material.uuid)
-      stats.textureBytes += getTextureBytesFromMaterial(material)
-    }
-  }
+  // applyStats(stats) {
+  //   const geometry = getGeometry(this._type)
+  //   if (geometry && !stats.geometries.has(geometry.uuid)) {
+  //     stats.geometries.add(geometry.uuid)
+  //     stats.triangles += getTrianglesFromGeometry(geometry)
+  //   }
+  //   // const material = getMaterial()
+  //   if (material && !stats.materials.has(material.uuid)) {
+  //     stats.materials.add(material.uuid)
+  //     stats.textureBytes += getTextureBytesFromMaterial(material)
+  //   }
+  // }
 
   get type() {
     return this._type
@@ -559,8 +569,8 @@ export class Prim extends Node {
     if (this._color === value) return
     this._color = value
     if (this.handle) {
-      this.needsRebuild = true
-      this.setDirty()
+      this.handle.setColor(value)
+      // no rebuild needed!
     }
   }
 
@@ -575,8 +585,8 @@ export class Prim extends Node {
     if (this._emissive === value) return
     this._emissive = value
     if (this.handle) {
-      this.needsRebuild = true
-      this.setDirty()
+      this.handle.setEmissive(value)
+      // no rebuild needed!
     }
   }
 
@@ -623,8 +633,8 @@ export class Prim extends Node {
     if (this._emissiveIntensity === value) return
     this._emissiveIntensity = value
     if (this.handle) {
-      this.needsRebuild = true
-      this.setDirty()
+      this.handle.setEmissiveIntensity(value)
+      // no rebuild needed!
     }
   }
 
@@ -1159,4 +1169,14 @@ function getGeometryConfig(type, requestedSize) {
   }
 
   return { size, scaleOffset }
+}
+
+function quantizeOpacity(opacity) {
+  // For fully opaque, keep it exact
+  if (opacity >= 0.99) return 1
+  // For nearly transparent, keep it at 0
+  if (opacity <= 0.01) return 0
+  // Quantize to 20 steps (0.05 increments)
+  // This gives smooth enough transitions while limiting materials to ~20 variants
+  return Math.round(opacity * 20) / 20
 }
